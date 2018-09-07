42c5ffc859ebb5e10ae60a42dfb65ad6adbd5b67
lxy <lixiaoyu283284@163.com>
2018-09-07 14:30:14 +0800

    fix bug in show_mxnet_graph, could convert mxnet to caffe for insightface

--- find_caffe.py
+++ find_caffe.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
     import os, sys
     curr_path = os.path.abspath(os.path.dirname(__file__))
-    sys.path.append(os.path.join(curr_path, "/home/chen/Documents/Python/toolbox/caffe-org/python"))
+    sys.path.append(os.path.join(curr_path, "/home/lxy/caffe/python"))
     import caffe
